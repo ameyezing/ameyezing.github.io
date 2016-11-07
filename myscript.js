365a20262a2c628fa50c1fa4d7f316c88e6f151b
ameyezing <simon.ameye@hotmail.fr>
2016-11-07 15:23:04 +0800

    hey
    e

---
+++ myscript.js
@@ -0,0 +1,5 @@
+var data = [
+{"english":"computer", "chinese":"LAISSE TOMBER 1", "pinyin":"dianao"},
+{"english":"i", "chinese":"LAISSE TOMBER 2", "pinyin":"wo"},
+{"english":"weather", "chinese":"LAISSE TOMBER 3", "pinyin":"tianqi"}
+];
