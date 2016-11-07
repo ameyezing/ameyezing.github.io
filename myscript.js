cb7f10c6995cf6c52a8aa7c68a7345cf16639822
ameyezing <simon.ameye@hotmail.fr>
2016-11-07 15:39:42 +0800

    s
    s

--- myscript.js
+++ myscript.js
@@ -3,3 +3,17 @@ var data = [
 {"english":"i", "chinese":"LAISSE TOMBER 2", "pinyin":"wo"},
 {"english":"weather", "chinese":"LAISSE TOMBER 3", "pinyin":"tianqi"}
 ];
+
+var i=0
+
+function tryme() {
+	i = Math.floor(Math.random() * data.length);
+	document.getElementById("chinese").innerHTML=data[i]['chinese'];
+	document.getElementById("english").innerHTML=data[i]['reveal'];
+	document.getElementById("pinyin").innerHTML=data[i]['reveal'];
+}
+
+function reveal(mydiv) {
+	document.getElementById(mydiv).innerHTML=data[i][mydiv];
+}
+
